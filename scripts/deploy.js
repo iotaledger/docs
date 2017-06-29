db466b58ea951177b0fd51a9ef2f8a7e257015cf
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-06-29 13:25:53 +0530

    Deploy docs to now via travis. We do this for every time travis builds code for a PR.

---
+++ scripts/deploy.js
@@ -0,0 +1,60 @@
+/*
+  Deploy to now and get the URL
+  Deploy to now via the token
+  Add a comment to the PR
+*/
+
+const shell = require('shelljs')
+const GitHubApi = require('github')
+const logger = console
+
+const { ZEIT_TOKEN, GH_TOKEN, PULL_REQUEST_ID } = process.env
+
+async function run() {
+  if (!PULL_REQUEST_ID || PULL_REQUEST_ID === '') return
+
+  // Change the team to ZEIT
+  const team = shell.exec(`now switch zeit -t ${ZEIT_TOKEN}`)
+  if (team.code !== 0) {
+    throw new Error(team.stderr)
+  }
+
+  // Deploy the app
+  const now = shell.exec(`now -t ${ZEIT_TOKEN}`, { silent: true })
+  if (now.code !== 0) {
+    throw new Error(now.stderr)
+  }
+
+  const deployUrl = now.stdout
+  logger.log('> App deployed to:', deployUrl)
+
+  // Create a GH comment
+  const github = new GitHubApi({
+    // optional
+    debug: true,
+    protocol: 'https',
+    headers: {
+      'user-agent': 'ZEIT docs on Travis' // GitHub is happy with a unique user agent
+    },
+    Promise,
+    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+  })
+
+  github.authenticate({
+    type: 'token',
+    token: GH_TOKEN
+  })
+
+  await github.issues.createComment({
+    owner: 'zeit',
+    repo: 'docs',
+    number: PULL_REQUEST_ID,
+    body: `You can view the modified docs at: ${deployUrl}`
+  })
+}
+
+run().catch(error => {
+  logger.error(error.stack)
+  process.exit(1)
+})
