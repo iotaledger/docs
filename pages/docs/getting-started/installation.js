0c829f9bf918d2d26258509d71f817fdc3970a60
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-09-09 23:33:35 +0530

    Make now-cli the best and first options to interact with Now.

--- pages/docs/getting-started/installation.js
+++ pages/docs/getting-started/installation.js
@@ -15,7 +15,27 @@ export default withDoc({
 
 In order to deploy to ${<Now color="#000"/>}, you need to install a small utility app. Let's see how you can do that.
 
-## Now Desktop (Recommended)
+## Now CLI
+
+${
+  <Image
+    src={`${IMAGE_ASSETS_URL}/docs/installation/now-cli.png`}
+    width={650}
+    height={330}
+  />
+}
+
+This is the command line interface for ${<Now color="#000"/>}. It's a pre-built binary [available](https://zeit.co/download#command-line) for all major operating systems and works without any dependencies.
+
+## Now via NPM
+
+This is exactly Now CLI but distributed via [NPM](https://www.npmjs.com/). This method is ideal if your operating system already has NPM support.
+
+${
+  <TerminalInput>npm install -g now</TerminalInput>
+}
+
+## Now Desktop
 
 ${
   <Image
@@ -26,36 +46,18 @@ ${
   />
 }
 
-This is a desktop app which is [available](https://zeit.co/download) for all of the major operation systems. It comes as an operation system status bar app.
+This is a desktop app which is [available](https://zeit.co/download) for Mac and Windows. It comes as an operation system status bar app.
 
 You can deploy apps, see notifications, and manage your account using an easy-to-use interface.
 It will update new versions automatically behind the scenes and you can always use the latest version of [Now Desktop](https://zeit.co/download).
-
-> Additionally, it comes with the \`now\` command line utility where you can interact with ${<Now color="#000"/>} using a terminal.
-
-## Now CLI
-
-This is the command line interface for ${<Now color="#000"/>}, especially built for [servers](https://zeit.co/download#command-line). It's a pre-built binary and works without any dependencies.
-
-Now CLI is the ideal way to interact with ${<Now color="#000"/>} inside Continues Integration(CI) services.
-
-## Now via NPM
-
-This is exactly Now CLI but distributed via [NPM](https://www.npmjs.com/). This method is ideal if your server environment already has NPM support.
-
-You can get this via the following command:
-
-${
-  <TerminalInput>npm install -g now</TerminalInput>
-}
 
 ## Open Source
 
 All of these apps are available under the open source MIT license. So, you can always check the source code and modify it as needed.
 You are always welcome to suggest new features and submit issues. Use following repositories:
 
-* Now Desktop - <https://github.com/zeit/now-desktop>
 * Now CLI - <https://github.com/zeit/now-cli>
+* Now Desktop - <https://github.com/zeit/now-desktop>
 * Now Client - <https://github.com/zeit/now-client>
 
 `)
