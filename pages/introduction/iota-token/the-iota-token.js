87320726fef290f629d338802a93cf3b61ca24f1
Lewis Freiberg <lewi@2pk.in>
2018-02-27 10:21:01 +0100

    Add token breakdown

--- pages/introduction/iota-token/the-iota-token.js
+++ pages/introduction/iota-token/the-iota-token.js
@@ -2,6 +2,13 @@ import markdown from 'markdown-in-js'
 import withDoc, { components } from '../../../lib/with-doc'
 
 import { Code } from '../../../components/text/code'
+import {
+  Table,
+  Row,
+  Cell,
+  TypeCell,
+  BoldCell
+} from '../../../components/api/table'
 import { lewi } from '../../../lib/data/team'
 
 // prettier-ignore
@@ -36,9 +43,54 @@ of IOTA's of \`2779530283277761\`. IOTA is specifically designed for machines,
 so this high supply makes IOTA optimal for tiny **nano-transactions** while still 
 keeping efficiency in mind. 
 
-It also nicely fits into the \`MAX_SAFE_INTEGER\` value in Javascript.
+This supply fits nicely into the \`MAX_SAFE_INTEGER\` value in Javascript.
+
+## Supply Etomology
+
+Since the number of tokens is quite high we adhere to the SI notation to write 
+in short hand to denote a large number of tokens. Here is a breakdown of the notation:
+
+${<Table>
+  <Row>
+    <BoldCell>Unit</BoldCell>
+    <Cell>Name</Cell>
+    <Cell>Amount</Cell>
+    <TypeCell>Powers</TypeCell>
+  </Row>
+  <Row>
+    <BoldCell>Pi</BoldCell>
+    <Cell>Peta IOTA</Cell>
+    <Cell>1.000.000.000.000.000</Cell>
+    <TypeCell>10<sup>15</sup></TypeCell>
+  </Row>
+  <Row>
+    <BoldCell>Ti</BoldCell>
+    <Cell>Terra IOTA</Cell>
+    <Cell>1.000.000.000.000</Cell>
+    <TypeCell>10<sup>12</sup></TypeCell>
+  </Row>
+  <Row>
+    <BoldCell>Gi</BoldCell>
+    <Cell>Giga IOTA</Cell>
+    <Cell>1.000.000.000</Cell>
+    <TypeCell>10<sup>9</sup></TypeCell>
+  </Row>
+  <Row>
+    <BoldCell>Mi</BoldCell>
+    <Cell>Mega IOTA</Cell>
+    <Cell>1.000.000</Cell>
+    <TypeCell>10<sup>6</sup></TypeCell>
+  </Row>
+  <Row>
+    <BoldCell>Ki</BoldCell>
+    <Cell>Kilo IOTA</Cell>
+    <Cell>1.000</Cell>
+    <TypeCell>10<sup>3</sup></TypeCell>
+  </Row>
+  </Table>}
 
 ## A Note in Trinary
+
 In this documentation you will often hear the phrase trinary-based which 
 is a reference to the unique data structure being utilized in IOTA. 
 Whenever we refer to trinary-based seeds, addresses, hashes etc., it means 
