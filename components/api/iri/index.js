d0eb0bd07518a10e09810bbcc3915248ed2b4121
Lewis Freiberg <lewi@2pk.in>
2018-03-31 11:45:59 +0800

    Remove unused files

--- components/api/iri/index.js
+++ components/api/iri/index.js
@@ -1,9 +1,7 @@
-import gettingStarted from './getting-started'
 import apiBasics from './api-basics'
 import endpoints from './endpoints'
 
 export default {
-  gettingStarted,
   apiBasics,
   endpoints
 }
