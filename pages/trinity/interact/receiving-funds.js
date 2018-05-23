48a61a1b0819b2c9b070848dbb760b58db5e36b7
Rajiv Shah <rajivshah1@icloud.com>
2018-05-22 20:33:46 -0400

    Add warning about Android devices

--- pages/trinity/interact/receiving-funds.js
+++ pages/trinity/interact/receiving-funds.js
@@ -11,10 +11,12 @@ export default withDoc({
   editUrl: 'pages/trinity/interact/receiving-funds.js',
 })(markdown(components)`
 
-Padd out
-
 [Image]
 
-You can now copy and paste the address, or share it with the QR code.
+In the Receive tab, you can generate addresses. Tap the "New Address" button to generate an address. Once it is generated,
+you can tap the address to copy it to the clipboard. You can also take a screenshot of the QR code and send it to a friend.
+
+> **Important**: On Android devices, any app can read the contents of the keyboard and modify them. Be careful when pasting
+> addresses into another app and ensure that the address you have pasted is the same one you originally copied.
 
 `)
--- pages/trinity/interact/sending-funds.js
+++ pages/trinity/interact/sending-funds.js
@@ -13,6 +13,9 @@ export default withDoc({
 
 To send funds, enter the receiving address and the amount to send.
 
+> **Important**: On Android devices, any app can read the contents of the keyboard and modify them. Be careful when pasting
+> addresses into Trinity and ensure that the address you have pasted is the same one you originally copied.
+
 ${
   <Image
     src={`${IMAGE_ASSETS_URL}/docs/trinity/send.jpg`}
@@ -22,15 +25,15 @@ ${
 }
 
 
-With multiple accounts in your Trinity wallet, be sure to select the 
-right account to send from in the dropdown menu. The amount to send 
-can be entered in IOTA or in the fiat currency of your choice. 
+With multiple accounts in your Trinity wallet, be sure to select the
+right account to send from in the dropdown menu. The amount to send
+can be entered in IOTA or in the fiat currency of your choice.
 
-Please pay close attention to the IOTA units you are sending (i, Mi, Gi, etc.). 
+Please pay close attention to the IOTA units you are sending (i, Mi, Gi, etc.).
 
 [Screenshot with highlighted button]
 
-Press the "IOTA units explained" at the bottom for further information. 
+Press the "IOTA units explained" at the bottom for further information.
 
 [Screenshot with IOTA Units up]
 
@@ -38,6 +41,6 @@ You can include a message by filling out the highlighted field.
 
 [Screenshot with highlighted field]
 
-To change the default currency, go to **Settings**. 
+To change the default currency, go to **Settings**.
 
 `)
