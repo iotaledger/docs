158301503e1dfa1a0a4d3e362b85bc1e4d7f87e6
Lewis Freiberg <lewi@2pk.in>
2018-07-02 07:19:18 +0300

    Remove API and old render funcs

--- components/api/iri/api-basics/index.js
+++
@@ -1,9 +0,0 @@
-import contentType from './content-type'
-import errors from './errors'
-import types from './types'
-
-export default {
-  contentType,
-  types,
-  errors
-}
--- components/api/iri/index.js
+++
@@ -1,7 +0,0 @@
-import apiBasics from './api-basics'
-import endpoints from './endpoints'
-
-export default {
-  apiBasics,
-  endpoints
-}
