dece25feb03196acd68febfe575dc891b05032a1
Jakub Cech <jakub.cech@iota.org>
2018-07-02 14:46:21 +0200

    Relative link change

--- pages/iri/usage/troubleshooting-iri.js
+++ pages/iri/usage/troubleshooting-iri.js
@@ -20,7 +20,7 @@ See details on the error and the resolution on ${<ExternalLink href="https://sta
 
 ## IRI Node is not synchronizing
 
-1. Make sure you have enough neighbors. 6-7 neighbors at the very least. See [Finding neighbors](../usage/finding-neighbours.js). After adding enough neighbors, make sure there is traffic flowing and give the node some time to sync. Use \`getNeighbors\` to see both incoming \`numberOfAllTransactions\` and outgoing \`numberOfSentTransactions\`. Full command example: \`curl http://localhost:14265 -X POST -H 'Content-Type: application/json' -H 'X-IOTA-API-Version: 1' -d '{"command": "getNeighbors"}'\` 
+1. Make sure you have enough neighbors. 6-7 neighbors at the very least. See [Finding neighbors](/iri/usage/finding-neighbours). After adding enough neighbors, make sure there is traffic flowing and give the node some time to sync. Use \`getNeighbors\` to see both incoming \`numberOfAllTransactions\` and outgoing \`numberOfSentTransactions\`. Full command example: \`curl http://localhost:14265 -X POST -H 'Content-Type: application/json' -H 'X-IOTA-API-Version: 1' -d '{"command": "getNeighbors"}'\` 
 2. Update to the ${<ExternalLink href="https://github.com/iotaledger/iri/releases" key="">latest version</ExternalLink>}  of IRI.
 3. Get more support by joining our ${<ExternalLink href="https://bit.ly/iota-invite" key="">discord</ExternalLink>} #fullnodes channel.
 
