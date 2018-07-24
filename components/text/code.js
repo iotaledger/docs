a73c450d5f5a18f01ea59d49110496a5dfb31557
Lewis Freiberg <lewi@2pk.in>
2018-07-24 14:41:57 +0300

    Add the highlighting

--- components/text/code.js
+++ components/text/code.js
@@ -1,42 +1,23 @@
-import PropTypes from 'prop-types'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { vs } from 'react-syntax-highlighter/styles/hljs'
 
-export const Code = ({ children, syntax }, { darkBg } = {}) => (
-  <pre className={(darkBg ? 'dark' : '') + (syntax ? ` ${syntax}` : '')}>
-    <code>{children}</code>
-    <style jsx>
-      {`
-        pre {
-          border: 1px solid #eaeaea;
-          padding: 20px;
-          margin: 40px 0;
-          white-space: pre;
-          overflow: auto;
-          -webkit-overflow-scrolling: touch;
-        }
-        code {
-          color: #bd10e0;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace,
-            serif;
-          font-size: 13px;
-          line-height: 20px;
-        }
-        pre.dark {
-          border-color: #333;
-        }
-        .dark code {
-          color: #fff;
-        }
-        .dark.shell code {
-          color: #50e3c2;
-        }
-      `}
-    </style>
-  </pre>
+export const Code = ({ children }) => (
+  <SyntaxHighlighter
+    language="javascript"
+    style={vs}
+    wrapLines={true}
+    customStyle={block}
+  >
+    {children}
+  </SyntaxHighlighter>
 )
 
-Code.contextTypes = {
-  darkBg: PropTypes.bool
+const block = {
+  border: '1px solid #eaeaea',
+  padding: '20px 20px 0',
+  whiteSpace: 'pre',
+  overflow: 'auto',
+  webkitOverflowScrolling: 'touch'
 }
 
 export const InlineCode = ({ children, noWrap }) => (
@@ -45,7 +26,7 @@ export const InlineCode = ({ children, noWrap }) => (
     <style jsx>
       {`
         code {
-          color: #bd10e0;
+          color: #343434;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace,
             serif;
